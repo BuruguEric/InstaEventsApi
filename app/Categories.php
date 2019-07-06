e9004e54a6c5fb98ec8639a64a55e8fde7091534
BuruguEric <ericgburugu@gmail.com>
2019-07-06 15:25:27 +0300

    categories posting

--- app/Categories.php
+++ app/Categories.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categories extends Model
 {
-    //
+    protected $table = 'categories';
+    protected $primaryKey = 'category_id';
+    protected $fillable = ['category_id','category','category_description','category_poster','created_at','updated_at'];
 }
