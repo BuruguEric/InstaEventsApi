bb91b56c9ba72e603a377a2acc35c59610e0f99d
BuruguEric <ericgburugu@gmail.com>
2019-07-07 22:02:39 +0300

    date and time datatype change done

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -52,7 +52,7 @@ class EventController extends Controller
      */
     public function updateEvent($id, Request $request)
     {
-        $event = Events::find($id)->update([json_encode(" " , $request->all())]);
+        $event = Events::find($id)->update([json_decode(" " , $request->all())]);
         
         $event->save();
         
