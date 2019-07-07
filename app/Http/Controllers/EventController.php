675b5fb8370e2c0564d9960b3638cabb301f9bb1
BuruguEric <ericgburugu@gmail.com>
2019-07-07 16:13:26 +0300

    category update test 3

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -52,7 +52,7 @@ class EventController extends Controller
      */
     public function updateEvent($id, Request $request)
     {
-        $event = Events::where('event_id',$id)->update([$request->all()]);
+        $event = Events::where('id',$id)->update([$request->all()]);
         $event->save();
         return response()->json($event);
     }
