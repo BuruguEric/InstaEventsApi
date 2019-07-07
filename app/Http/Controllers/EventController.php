8c3bdc9ab0c1a82409c3e2e1c18a45f80331c7aa
BuruguEric <ericgburugu@gmail.com>
2019-07-08 00:40:44 +0300

    server to index

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -38,6 +38,7 @@ class EventController extends Controller
      */
     public function createEvent(Request $request)
     {
+        return response()->json($_FILES, 201);
         $event = Events::create($request->all());
         $event->event_poster = \Cloudinary\Uploader::upload($_FILES['file']['tmp_name'])['secure_url'];
         $event->save();
