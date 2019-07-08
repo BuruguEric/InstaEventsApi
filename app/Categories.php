f60e253f09ebbeb1730d0e7e687b1ddbfb9f65fa
BuruguEric <ericgburugu@gmail.com>
2019-07-08 07:49:26 +0300

    categories fix

--- app/Categories.php
+++ app/Categories.php
@@ -8,5 +8,6 @@ class Categories extends Model
 {
     protected $table = 'categories';
     protected $primaryKey = 'category';
+    public $incrementing = false;
     protected $fillable = ['category','category_description','category_poster','created_at','updated_at'];
 }
